85a1b8d03202c8b805d4a267c7d5996881bcb2f9
fanta <fms294@nyu.edu>
2020-08-30 17:32:51 -0400

    Added login and register button to welcome

--- App.js
+++ App.js
@@ -13,15 +13,7 @@ import AppButton from './app/components/AppButton';
 export default function App() {
 
   return (
-    <View 
-      style={{ 
-        flex:1, 
-        justifyContent: "center",
-        alignItems:"center"
-        }}>
-
-      <AppButton title="login" />
-    </View>
+    <WelcomeScreen />
       
   );
 }
