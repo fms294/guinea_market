d52420b8c1c7230dd8e8434ca44e94df5b8844b1
fanta <fms294@nyu.edu>
2020-10-19 21:55:14 -0400

    added list and input component

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 
-import React from 'react';
-import { View} from 'react-native';
-import WelcomeScreen from './app/screens/WelcomeScreen';
-import ViewImageScreen from './app/screens/ViewImageScreen';
-import { MaterialCommunityIcons} from '@expo/vector-icons';
+import React, { useState } from 'react';
 
 import Screen from './app/components/Screen';
-import AcccountScreen from './app/screens/AccountScreen';
-import Icon from './app/components/Icon';
-import AppText from './app/components/AppText';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
-import ListIem from './app/components/ListItem';
-import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
-import ListingsScreen from './app/screens/ListingsScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
+
+import { Switch } from 'react-native';
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from './app/components/AppTextInput';
+
+const categories =[
+  {label: "Furniture", value:1},
+  {label: "Clothing", value:2},
+  {label: "Cameras", value:3},
+]
 
 export default function App() {
+  const [category, setCategory] = useState(categories[0]);
 
   return (
-    <ListingsScreen />
-      
+    <Screen>
+        <AppPicker
+          selectedItem={category}
+          onSelectItem={item => setCategory(item)}
+         items={categories} icon="apps" 
+         placeholder="Category" 
+         />
+        <AppTextInput icon="email" placeholder="Email" />
+    </Screen>     
   );
 }
 
