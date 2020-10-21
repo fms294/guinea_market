8737f4c8271c649d35b8030a9ed29a449cd598b7
fanta <fms294@nyu.edu>
2020-10-20 22:32:45 -0400

    Added form Formik and Yup for validation

--- App.js
+++ App.js
@@ -1,31 +1,12 @@
 
 import React, { useState } from 'react';
+import LoginScreen from './app/screens/LoginScreen';
 
-import Screen from './app/components/Screen';
-
-import { Switch } from 'react-native';
-import AppPicker from './app/components/AppPicker';
-import AppTextInput from './app/components/AppTextInput';
-
-const categories =[
-  {label: "Furniture", value:1},
-  {label: "Clothing", value:2},
-  {label: "Cameras", value:3},
-]
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
+ 
   return (
-    <Screen>
-        <AppPicker
-          selectedItem={category}
-          onSelectItem={item => setCategory(item)}
-         items={categories} icon="apps" 
-         placeholder="Category" 
-         />
-        <AppTextInput icon="email" placeholder="Email" />
-    </Screen>     
+    <LoginScreen />
   );
 }
 
