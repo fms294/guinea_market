2617bdb186800b2a5c8275b0e98b60169dbbfafc
fanta <fms294@nyu.edu>
2020-10-25 02:52:23 -0400

    added image Picker

--- app/components/Card.js
+++ app/components/Card.js
@@ -3,7 +3,7 @@ import {View, StyleSheet, Image } from 'react-native';
 
 
 import colors from '../config/colors';
-import AppText from './AppText';
+import AppText from './Text';
 
 function Card({title, subTitle, image}){
     return(
