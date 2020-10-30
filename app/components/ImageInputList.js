4f9276810de5216d162ad8bacbc5dc4e2f8eada8
fanta <fms294@nyu.edu>
2020-10-29 21:31:09 -0400

    added formImagePicker

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, { useRef } from 'react';
+import {View, StyleSheet, ScrollView} from 'react-native';
 import ImageInput from './ImageInput';
 
+
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+    const scrollVIew = useRef();
+    
     return (
-         <View style={styles.container} style={styles.image}>
-             { imageUris.map((uri) => (
-            <View   key={uri} >
-               <ImageInput 
-                    imageUri={uri} 
-                    onChangeImage={() => onRemoveImage(uri)} 
-                />
-            </View>
-                ))}
-             <ImageInput onChangeImage={ uri => onAddImage(uri)} />
-         </View>
+        <View>
+            <ScrollView 
+                ref={scrollVIew} 
+                horizontal={true} 
+                onContentSizeChange={() => scrollVIew.current.scrollToEnd()}
+            >
+                <View style={styles.container} style={styles.image}>
+                    { imageUris.map((uri) => (
+                        <View   key={uri} >
+                    <ImageInput 
+                            imageUri={uri} 
+                            onChangeImage={() => onRemoveImage(uri)} 
+                            />
+                    </View>
+                        ))}
+                    <ImageInput onChangeImage={ uri => onAddImage(uri)} />
+                </View>
+            </ScrollView>
+        </View>
   );
 }
 
