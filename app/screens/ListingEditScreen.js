dfb71a4cbed1957ee5b5c8d71fe638a2b4b96164
fanta <fms294@nyu.edu>
2020-10-29 22:27:00 -0400

    added region component

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -12,6 +12,7 @@ import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/FormImagePicker";
 import useLocation from "../hooks/useLocation";
+import RegionPickerItem from "../components/RegionPickerItem";
 
 
 const validationSchema = Yup.object().shape({
@@ -19,6 +20,7 @@ const validationSchema = Yup.object().shape({
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  region: Yup.object().required().nullable().label("Region"),
   images: Yup.array().required().min(1,"Please select at list one image")
 });
 
@@ -26,6 +28,15 @@ const categories = [
   { label: "Furniture", value: 1, backgroundColor:"red" , icon: "apps"},
   { label: "Clothing", value: 2 , backgroundColor:"green", icon: "email"},
   { label: "Camera", value: 3 , backgroundColor:"blue", icon: "lock"},
+];
+
+const regions = [
+  { label: "Conakry", value: 1, backgroundColor:"red" , icon: "apps"},
+  { label: "Kindia", value: 2, backgroundColor:"green", icon: "email"},
+  { label: "Boke", value: 3 , backgroundColor:"blue", icon: "lock"},
+  { label: "Labe", value: 4, backgroundColor:"red" , icon: "apps"},
+  { label: "Kankan", value: 5, backgroundColor:"green", icon: "email"},
+  { label: "Siguiri", value: 6 , backgroundColor:"blue", icon: "lock"},
 ];
 
 function ListingEditScreen() {
@@ -38,6 +49,7 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          region:null,
           images:[]
         }}
         onSubmit={(values) => console.log(location)}
@@ -57,6 +69,13 @@ function ListingEditScreen() {
           name="category"
           PickerItemComponent={CategoryPickerItem} 
           placeholder="Category" 
+        />
+        <Picker 
+          items={regions} 
+          numberOfColumns={3}
+          name="regions"
+          PickerItemComponent={RegionPickerItem} 
+          placeholder="Region" 
         />
         <FormField
           maxLength={255}
