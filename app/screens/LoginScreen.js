de48a7b18ad11594eb0a89cdab6e5d24e4f0b8ca
fanta <fms294@nyu.edu>
2020-11-07 22:11:22 -0500

    extracting the user form authToken

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useState }  from 'react';
 import {StyleSheet, Image } from 'react-native';
 import * as Yup from 'yup';
+import jwtDecode from 'jwt-decode';
 
 import Screen from '../components/Screen';
 import { AppForm, AppFormField, ErrorMessage, SubmitButton} from '../components/forms'
@@ -18,7 +19,8 @@ function LoginScreen(props) {
         const result = await authApi.login(email, password);
         if(!result.ok) return setLoginFailed(true);
         setLoginFailed(false);
-        console.log(result.data);
+        const user = jwtDecode (result.data);
+        console.log(user);
     }
     return (
         <Screen style={styles.container}>
