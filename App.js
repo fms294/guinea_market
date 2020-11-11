7c8fc14052efa39e21d9d7b10e801a58ca7fee4d
fanta <fms294@nyu.edu>
2020-11-10 21:30:03 -0500

    Refactoring

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import jwtDecode from 'jwt-decode';
+
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
@@ -10,18 +10,22 @@ import OfflineNotice from './app/components/offlineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
+import { AppLoading } from 'expo';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState();
 
-  const restoreToken = async() =>{
-    const token = await authStorage.getToken();
-    if(!token) return ;
-    setUser(jwtDecode(token));
+  const restoreUser = async() =>{
+    const user = await authStorage.getUser();
+    if(!user) setUser(user);
   }
-  useEffect(( )=> {
-      restoreToken()
-  }, []);
+
+  if(!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)}/>
+    )
+  
   return(
     <AuthContext.Provider value={{user, setUser }}>
       <OfflineNotice />
